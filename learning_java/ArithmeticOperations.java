1f81bd10b88fc29aa3b6eb01b0b5cde271266281
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-01-27 17:42:31 +0300

    Update ArithmeticOperations

--- learning_java/ArithmeticOperations.java
+++ learning_java/ArithmeticOperations.java
@@ -1,5 +1,8 @@
 public class ArithmeticOperations {
     public static void main(String[] args) {
-        
+        int a = 10; // Обьявили переменную "a"
+        int b = 20; // Обьявили переменную "b"
+        int result = a + b; // Сложение переменных
+        System.out.println("Сумма: " + result); //Вывод результата
     }
 }
