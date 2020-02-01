7465d651d193b23cd5f877c461da3385d5756ccb
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-01 18:27:11 +0300

    Update LogicalOperetions

--- learning_java/LogicalOperations.java
+++ learning_java/LogicalOperations.java
@@ -3,6 +3,7 @@ public class LogicalOperations {
        boolean b_1 = true; //Создали переменную "b_1"
        boolean b_2 = false; //Создали переменную "b_2"
        System.out.println(b_1 + " && " + b_2  + " = " + (b_1 && b_2)); //Вывод результата сравнения "логическо И"
-        
+       System.out.println(b_1 + " || " + b_2  + " = " + (b_1 || b_2)); //Вывод результата сравнения "логическое ИЛИ"
+       
     }
 }
