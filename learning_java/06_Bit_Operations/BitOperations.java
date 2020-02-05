4497b73b2e0d89c9079e548bc49c6286e978be89
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-05 21:13:49 +0300

    Update BitOperations

--- learning_java/06_Bit_Operations/BitOperations.java
+++ learning_java/06_Bit_Operations/BitOperations.java
@@ -1,5 +1,10 @@
 public class BitOperations {
     public static void main(String[] args) {
-        
+        int a = 4; //Создали переменную "a"
+        int b = 7; //Создали перемунную "b"
+        String s_a = Integer.toBinaryString(a); //Преобразуем переменную "a" в двоичную
+        String s_b = Integer.toBinaryString(b); //Преобразуем переменную "b" в двоичную
+        System.out.println("s_a = " + s_a); //Вывод результата
+        System.out.println("s_a = " + s_b); //Вывод результата
     }
 }
