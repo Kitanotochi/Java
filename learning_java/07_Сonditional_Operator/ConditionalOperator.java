34fe23df63d09b7a494aae3096aef03a368c6d5f
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-11 20:25:17 +0300

    Update ConditionalOperator.java

--- learning_java/07_Сonditional_Operator/ConditionalOperator.java
+++ learning_java/07_Сonditional_Operator/ConditionalOperator.java
@@ -3,11 +3,13 @@ public class ConditionalOperator {
     public static void main(String[] args) {
         int a = 7; //Создание переменной
         int b = 9; //Создание переменной
-        if (a < b) { //Если 
+        if (a < b) { //Если
             System.out.println("a < b"); //Вывод результата
         }
         else { //Иначе
             System.out.println("a >= b");//Вывод результат
         }
+        String str = a < b ? "a < b" : "a >= b"; //Тернатный оператор
+        System.out.println(str); //Вывод результата
     }
 }
