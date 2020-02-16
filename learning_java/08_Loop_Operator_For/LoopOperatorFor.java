1b269a753174cad986958c6f30662b4049cd4eb0
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-16 15:58:27 +0300

    Update LoopOperatorFor.java

--- learning_java/08_Loop_Operator_For/LoopOperatorFor.java
+++ learning_java/08_Loop_Operator_For/LoopOperatorFor.java
@@ -1,6 +1,8 @@
 public class LoopOperatorFor {
     //Оператор цикла
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 100; i++) { //Объявляем переменную целого типа и присваиваем значение ноль. Переменная увеличивается на еденицу
+            System.out.println(i + " "); //Вывод результата
+        }
     }
 }
