1761f8263be134b9200d819c56eb631b4e822b13
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-17 18:39:00 +0300

    Create LoopOperatorWhile.java

---
+++ learning_java/09_Loop_Operator_While/LoopOperatorWhile.java
@@ -0,0 +1,10 @@
+public class LoopOperatorWhile {
+//Оператор цикла while
+    public static void main(String[] args){
+        int i = 0;
+        while (i < 100) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
