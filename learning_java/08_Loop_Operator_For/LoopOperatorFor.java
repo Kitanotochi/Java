6d72ddb5c19736b9e4a893ea7024f30d91ec4ed3
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-17 18:41:36 +0300

    Update LoopOperatorFor.java

--- learning_java/08_Loop_Operator_For/LoopOperatorFor.java
+++ learning_java/08_Loop_Operator_For/LoopOperatorFor.java
@@ -4,5 +4,15 @@ public class LoopOperatorFor {
         for (int i = 0; i < 100; i++) { //Объявляем переменную целого типа и присваиваем значение ноль. Переменная увеличивается на еденицу
             System.out.println(i + " "); //Вывод результата
         }
+        System.out.println();
+        for (int i = 0; i < 100; i++)
+            if (i % 2 == 0) System.out.print(i + " ");
+        int summa = 0;
+        int n = 10;
+        for (int i = 0; i <= n; i += 2) {
+            summa += i;
+        }
+        System.out.println();
+        System.out.println("Сумма четных чисел от 0 до " + n + " = " + summa);
     }
 }
