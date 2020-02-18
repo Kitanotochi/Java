acc09b8b8f0c657d80e1be48adc9f07265ade8df
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-18 19:36:57 +0300

    Update LoopOperatorDoWhile.java

--- learning_java/10_Loop_Operator_Do-While/LoopOperatorDoWhile.java
+++ learning_java/10_Loop_Operator_Do-While/LoopOperatorDoWhile.java
@@ -8,5 +8,12 @@ public class LoopOperatorDoWhile {
         do {
             System.out.println("Мы в цикле do-while");
         } while (a > 10);
+        a = 0;
+        while (true) {
+            a++;
+            if (a == 10) break;
+            if (a % 2 == 0) continue;
+            System.out.println(a);
+        }
     }
 }
