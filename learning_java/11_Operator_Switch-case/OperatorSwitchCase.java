67d9c8eb8e3382acf01e2b1ba1e8dfabfa8e78e2
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-19 18:45:47 +0300

    Update OperatorSwitchCase.java

--- learning_java/11_Operator_Switch-case/OperatorSwitchCase.java
+++ learning_java/11_Operator_Switch-case/OperatorSwitchCase.java
@@ -1,6 +1,21 @@
 public class OperatorSwitchCase{
     //Оператор Switch-case
     public static void main(String[] args) {
-        
+        int a = 1;
+        String s;
+        switch (a) {
+            case 1:
+                s = "Один";
+                break;
+            case 2:
+                s = "Два";
+                break;
+            case 3:
+                s = "Три";
+                break;
+            default:
+                s = "Другое число";
+        }
+        System.out.println(s);
     }
 }
