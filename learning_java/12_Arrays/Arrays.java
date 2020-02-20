9319a5db6cff430df2d8ae9d986fa014885c1872
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-20 19:54:15 +0300

    Update Arrays.java

--- learning_java/12_Arrays/Arrays.java
+++ learning_java/12_Arrays/Arrays.java
@@ -1,6 +1,13 @@
 public class Arrays {
 //Одномерные массивы
     public static void main(String[] args) {
-        
+        int [] arr = new int[5]; //Создаем массив
+        arr[0] = 10;
+        arr[1] = 5;
+        arr[2] = 30;
+        arr[3] = 10;
+        arr[4] = 12;
+        System.out.println("Элемент с индексом 1 = " + arr[1]); //Вывод результата
+
     }
 }
