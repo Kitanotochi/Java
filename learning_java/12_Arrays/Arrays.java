f29e2edc86813a995521c0aadb7238b5a4152d62
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-24 19:22:20 +0300

    Update Arrays.java

--- learning_java/12_Arrays/Arrays.java
+++ learning_java/12_Arrays/Arrays.java
@@ -1,7 +1,7 @@
 public class Arrays {
 //Одномерные массивы
     public static void main(String[] args) {
-        int [] arr = new int[5]; //Создаем массив
+        int [] arr = new int[5]; //Создаем массив со значением "int"
         arr[0] = 10;
         arr[1] = 5;
         arr[2] = 30;
@@ -13,7 +13,22 @@ public class Arrays {
         for (int i = 0; i < d_arr.length; i++)
             d_arr[i] = Math.random();
         for (int i = 0; i < d_arr.length; i++)
-            System.out.println(d_arr[i]);
+            System.out.println(d_arr[i]); //Вывод результата
+        
+        String[] s_arr = {"Игорь", "Олег", "Александр"}; //Создали массив со значением "string"
+        for (int i = 0; i < s_arr.length; i++)
+            System.out.println(s_arr[i]); //Вывод результата
+
+        int max = arr[0]; //Узнаем максимальное значение массива "arr"
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] > max) max = arr[i];
+                System.out.println("Максимальное значение  массиве arr: " + max); //Вывод результата
+
+                for (int a : arr) { //Создали переменную "а", в которую будем помещать элементы массива при каждой иттерации
+                    System.out.println(a);
+                }
+            }
+
 
     }
 }
