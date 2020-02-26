a2b032cde40762a787e1f3e569c81cd218be90ae
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-26 08:52:29 +0300

    Update MultiArrays.java

--- learning_java/13_Multi_Arrays/MultiArrays.java
+++ learning_java/13_Multi_Arrays/MultiArrays.java
@@ -1,6 +1,14 @@
 public class MultiArrays {
     //Многомерные массивы
     public static void main(String[] args) {
-        
+        char [] [] arr = new char [2] [2]; //Создаем многомерный массив
+        arr [0] [0] = 'a'; //Обращаемся к элементу с индексом ноль
+        arr [0] [1] = 'b';
+        arr [1] [0] = 'c';
+        arr [1] [1] = 'd';
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr[i].length; j++)
+                System.out.println("Элемент с индексом " + i + ", " + j + " = " + arr[i] [j]); //Вывод результата
+
     }
 }
