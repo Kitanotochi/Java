923b52673c4931f0f6467925dcd462750bc6c46f
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-29 19:19:14 +0300

    Update MultiArrays.java

--- learning_java/13_Multi_Arrays/MultiArrays.java
+++ learning_java/13_Multi_Arrays/MultiArrays.java
@@ -15,9 +15,9 @@ public class MultiArrays {
                     for (int j = 0; j < table[i].length; j++)
                         table[i] [j] = (i + 1) * (j + 1);
                 }
-                System.out.println(" 1 2 3 4 5 6 7 8 9");
+                System.out.println("   1  2  3  4  5  6  7  8  9");
                 for (int i = 0; i < table.length; i++) {
-                    System.out.print((i + 1) + " ");
+                    System.out.print((i + 1) + "  ");
                     for (int j = 0; j < table[i].length; j++) {
                         System.out.print(table[i][j] + " "); 
                         if (table[i][j] < 10) System.out.print(" "); 
