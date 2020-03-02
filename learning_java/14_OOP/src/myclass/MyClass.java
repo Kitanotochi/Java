7bf8725afe27680cbe6d647bd79361e8fb8ea057
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-03-02 21:00:51 +0300

    Update MyClass.java

--- learning_java/14_OOP/src/myclass/MyClass.java
+++ learning_java/14_OOP/src/myclass/MyClass.java
@@ -2,6 +2,12 @@ package myclass;
 
 public class MyClass {
     public static void main(String[] args) {
+    Person p = new Person("Михаил", "Москва", 40);
+    System.out.println(p.name);
+    System.out.println(p.city);
+    System.out.println(p.age);
+    Person [] p_arr = new Person[3];
+    p_arr[0] = new Person("Игорь", "Омск", 30);
     
     }
 }
