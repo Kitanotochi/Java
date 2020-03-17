992dbdd22d80d786569435e8583f5524acfe6847
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-03-17 21:07:53 +0300

    Update Method.java

--- learning_java/15_Reboot_methods/src/method/Method.java
+++ learning_java/15_Reboot_methods/src/method/Method.java
@@ -2,6 +2,7 @@ package method;
 
 public class Method {
     public static void main(String[] args) {
-
+        Person p = new Person("Igor", " Omsk", 20);
+        System.out.println(p.getTextInfo());
     }
 }
