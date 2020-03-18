42d0445d74578915fac93dbf8cd8b34a64c4e896
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-03-18 21:48:45 +0300

    Update Method.java

--- learning_java/15_Reboot_methods/src/method/Method.java
+++ learning_java/15_Reboot_methods/src/method/Method.java
@@ -4,8 +4,14 @@ public class Method {
     public static void main(String[] args) {
         Person p = new Person("Igor", "Omsk", 20);
         System.out.println(p.getTextInfo());
+        p.setName("German");
+        p.setCity("London");
+        p.setAge(20);
         System.out.println(p.getName());
         System.out.println(p.getCity());
         System.out.println(p.getAge());
+
+        Person pNew = new Person("Alex", "Moscow", 20);
+        p.whoIsOlder(pNew);
     }
 }
