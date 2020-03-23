0e6d575f2fffa362e29069d67d6467098f849be8
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-03-23 22:46:15 +0300

    Summary_access

---
+++ learning_java/16_Access_modifiers/src/access/Access.java
@@ -0,0 +1,9 @@
+package access;
+
+public class Access {
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println(p.name);
+        System.out.println(p.city);
+    }
+}
---
+++ learning_java/16_Access_modifiers/src/access/Person.java
@@ -0,0 +1,46 @@
+package access;
+
+public class Person {
+        public String name;
+        protected String city;
+        private int age;
+
+        public Person(String name, String city, int age) {
+            this.name =name;
+            this.city = city;
+            this.age = age;
+        }
+        public Person() { //Перегрузка конструкторов
+            this("Default name", "Default city", 18);
+        }
+        public String getTextInfo() { //Метод GET
+            return "Имя " + this.name + "; Город " + this.city + "; Возраст " + this.age;
+        }
+        public String getName() {
+
+            return this.name;
+        }
+        public String getCity() {
+
+            return this.city;
+        }
+        public int getAge() {
+
+            return this.age;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public void setCity(String city) {
+            this.city = city;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public void whoIsOlder(Person p) {
+            if (this.age > p.age) System.out.println(this.name + " старше чем, " + p.name);
+            else if (this.age == p.age) System.out.println(this.name + " и " + p.name + " росвесники!");
+            else System.out.println(this.name + " младше чем, " + p.name);
+        }
+    }
+
