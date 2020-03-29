fd91e51684cff90afc2b22e1d7101ad769034275
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-03-29 20:31:58 +0300

    Update Access.java

--- learning_java/16_Access_modifiers/src/access/Access.java
+++ learning_java/16_Access_modifiers/src/access/Access.java
@@ -4,6 +4,7 @@ public class Access {
     public static void main(String[] args) {
         Person p = new Person();
         System.out.println(p.name);
-        System.out.println(p.city);
+        System.out.println(p.city); // Работает, поскольку один пакет
+        // System.out.println(p.age); - Ошибка
     }
 }
