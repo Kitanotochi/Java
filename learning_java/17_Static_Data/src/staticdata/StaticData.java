92dfef03e6be47d1a02116e2bf088043f257067b
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-04-01 21:30:06 +0300

    Summary

--- learning_java/17_Static_Data/src/staticdata/StaticData.java
+++ learning_java/17_Static_Data/src/staticdata/StaticData.java
@@ -3,5 +3,6 @@ package staticdata;
 public class StaticData {
     public static void main(String[] args) {
         new Person();
+        System.out.println("Созданно объектов: " + Person.count);
     }
 }
