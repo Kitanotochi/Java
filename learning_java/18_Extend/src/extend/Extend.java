6ae10f4c397c0f432f7e0d7bb96d333723939cc9
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-04-04 16:26:38 +0300

    Create Extend.java

---
+++ learning_java/18_Extend/src/extend/Extend.java
@@ -0,0 +1,8 @@
+package extend;
+
+public class Extend {
+    public static void main(String[] args) {
+        Programmer p = new Programmer("Michael", "Omsk", 45, 20);
+        System.out.println(p.getTextInfo());
+    }
+}
