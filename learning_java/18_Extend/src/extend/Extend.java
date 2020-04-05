c83e12d3b4b921e358126b293aa8220f89f75028
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-04-05 21:10:45 +0300

    Update Extend.java

--- learning_java/18_Extend/src/extend/Extend.java
+++ learning_java/18_Extend/src/extend/Extend.java
@@ -4,5 +4,9 @@ public class Extend {
     public static void main(String[] args) {
         Programmer p = new Programmer("Michael", "Omsk", 45, 20);
         System.out.println(p.getTextInfo());
+
+        Worker w = new Worker();
+        w.setSallary(20000);
+        System.out.println(w.getTextInfo());
     }
 }
