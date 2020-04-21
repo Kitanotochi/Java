5b0c7c72a33a16b73ee59a39c07d0b99d780dbb6
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-04-21 20:28:15 +0300

    Summary

---
+++ learning_java/19_Subclass/src/subclass/Firm.java
@@ -0,0 +1,11 @@
+package subclass;
+
+public class Firm {
+    public String nameFirm;
+    public Director director;
+
+    public Firm(String nameFirm, String nameDirector, int age) {
+        
+    }
+
+}
