53fae788329a0f7dbdeaf55e41013b7ea3ae67c4
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-05-12 20:21:29 +0300

    Update SubClass.java

--- learning_java/19_Subclass/src/subclass/SubClass.java
+++ learning_java/19_Subclass/src/subclass/SubClass.java
@@ -3,7 +3,8 @@ package subclass;
 public class SubClass {
     public static void main(String[] args) {
         Firm f = new Firm("My Firm", "Igor", 30);
-        
+        System.out.println(f.director.name);
+        System.out.println("Директор является пенсионером " + f.director.isPensioner());
 
 
     }
